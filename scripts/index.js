c9e3f78e6dd0cb05cba144d15bec7005cd5fba8e
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-04-22 17:22:41 -0300

    Adicionado arquivo JS e respectivas funções e finalizando estilização da tela de resultado

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+function jogar(jogada) {
+    console.log(jogada)
+}
+
+function jogarNovamente() {
+    console.log("jogar novamente")
+    
+}
+
+function zerarPlacar() {
+    console.log("zerar placar")
+    
+
+}
