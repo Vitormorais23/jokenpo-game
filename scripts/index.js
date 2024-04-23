0964834109245307ef450b3b99232f0fc47bace2
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-04-23 11:00:20 -0300

    Obter jogada da maquina de forma aleatória

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,33 @@
 function jogar(jogada) {
-    console.log(jogada)
+
+    let jogadaMaquina = obterJogadaMaquina()
+
+    console.log(`Jogador: ${jogada} VS Maquina: ${jogadaMaquina}`)
 }
+
+function obterJogadaMaquina() {
+    // Criar um array com opções disponiveis
+    let options = ['pedra', 'papel', 'tesoura']
+
+    // Gerar numero alaátorio entre 0 e 3 onde o 3 não entra na conta pois essa função será gerada até o 2 
+    let position = getRandom(0, 3)
+
+    // Retornar a jogada da maquina
+    return options[position]
+}
+
+function getRandom(min, max) {
+    // min = Math.ceil(min); COMO ESTOU PASSANDO VALORES INTEIROS NÃO PRECISO DESSAS VARIÁVEIS
+    // max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+  
+  
 
 function jogarNovamente() {
     console.log("jogar novamente")
-    
 }
 
 function zerarPlacar() {
     console.log("zerar placar")
-    
-
 }
